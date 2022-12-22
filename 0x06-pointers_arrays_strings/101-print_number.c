e57d3b014fd247a1fbe202a06b727000634b870e
wizleriq <wisdomndata@gmail.com>
2022-12-22 13:10:11 -0800

    good task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,34 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-  * print_number - print numbers chars
-  * @n: integer params
-  * Return: 0
- **/
-
-void print_number(int n)
+ *  main - prints Buzz each numbers of 3 and 5.
+ *  Return: Always 0.
+ */
+int main(void)
 {
-	unsigned int n1;
+	int n;
 
-	n1 = n;
-
-	if (n < 0)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		_putchar('-');
-		n1 = -n;
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
 	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	printf("\n");
+	return (0);
 }
