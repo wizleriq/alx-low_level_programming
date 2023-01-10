417ee81bf930e0c40047cec321ab69bf79ee6f5d
wizleriq <wisdomndata@gmail.com>
2023-01-10 04:23:47 -0800

    task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,17 +12,16 @@ char *argstostr(int ac, char **av)
 	char *s;
 
 	if (ac == 0 || av == NULL)
-		return (NULL);
+	return (NULL);
 	for (i = 0; (i < ac); i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			l++;
-		        l++;
 	}
 
 	s = malloc(sizeof(char) * l + 1);
 	if (s == NULL)
-		return (NULL);
+	return (NULL);
 	for (x = 0; x < ac ; x++)
 	{
 		for (y = 0; av[x][y] != '\0'; y++)
